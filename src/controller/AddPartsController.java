c3d92f4db6c8a259fd7601e01304eae231807333
inquisitivejester <76171234+inquisitivejester@users.noreply.github.com>
2021-12-19 07:48:05 -0600

    Added all fxid's and to main screen methods.  Tested and everything seems to work.

--- src/controller/AddPartsController.java
+++ src/controller/AddPartsController.java
@@ -1,11 +1,49 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddPartsController implements Initializable {
+    public RadioButton in_house_radio_button;
+    public Label name_label;
+    public RadioButton outsourced_radio_button;
+    public Label machine_id_company_name_label;
+    public Button add_part_save_button;
+    public Button add_part_cancel_button;
+
+    private void toMainScreen(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainScene.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1200, 600);
+        stage.setTitle("Main Screen");
+        stage.setScene(scene);
+        stage.show();
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){}
+
+    public void onInHouse(ActionEvent actionEvent) {
+    }
+
+    public void onOutSourced(ActionEvent actionEvent) {
+    }
+
+    public void onSave(ActionEvent actionEvent) {
+    }
+
+    public void onCancel(ActionEvent actionEvent) throws IOException {
+        toMainScreen(actionEvent);
+    }
 }
